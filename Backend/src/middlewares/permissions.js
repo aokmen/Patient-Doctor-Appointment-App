28cb1a6629f023fd36ec8f53e9eee474303944d1
Habip Hakkioglu <h.hakkioglu@icloud.com>
2024-01-16 17:42:08 +0100

    permissions

---
+++ Backend/src/middlewares/permissions.js
@@ -0,0 +1,44 @@
+"use strict"
+
+// Middleware: permissions
+
+module.exports = {
+
+    isLogin: (req, res, next) => {
+
+        if (req.admin && req.admin.isActive) {
+
+            next()
+
+        } else {
+
+            res.errorStatusCode = 403
+            throw new Error('NoPermission: You must login.')
+        }
+    },
+
+    isStaff: (req, res, next) => {
+
+        if (req.admin && req.admin.isActive && req.admin.isStaff) {
+
+            next()
+
+        } else {
+
+            res.errorStatusCode = 403
+            throw new Error('NoPermission: You must log in and be staff.')
+        }
+    },
+    isAdmin: (req, res, next) => {
+
+        if (req.admin && req.admin.isActive && req.admin.isAdmin) {
+
+            next()
+
+        } else {
+
+            res.errorStatusCode = 403
+            throw new Error('NoPermission: You must login and be admin.')
+        }
+    }
+}
