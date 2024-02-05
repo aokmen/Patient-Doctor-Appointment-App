d2acaa7b81c41edc2fc5736380dae53cc27c5498
Abdullah <129761775+aokmen@users.noreply.github.com>
2024-02-05 09:52:22 +0100

    Update tailwind.config.js

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
     extend: {
       colors: {
         "main-dark-blue" :"#38638D",
-        "main-light-blue" :"#B7D8F8",
+        "main-light-blue" :"#F1F7FE",
+        "main-light-blue2" :"#B7D8F8",
         "main-blue" :"#5999D7",
         "main-green" :"#59D4D4",
         
