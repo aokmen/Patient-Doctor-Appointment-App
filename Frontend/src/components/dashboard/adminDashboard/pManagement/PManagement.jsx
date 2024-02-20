a6afc9ae667fd9b4c09c956dd46e8492fd0bea42
aokmen <abdullah.okmen.11@gmail.com>
2024-02-20 09:00:12 +0100

    patient_profile added and updated

--- Frontend/src/components/dashboard/adminDashboard/pManagement/PManagement.jsx
+++ Frontend/src/components/dashboard/adminDashboard/pManagement/PManagement.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+
+import React, { useEffect, useRef, useState } from 'react'
+import "./dManagement.css"
+import { useSelector } from 'react-redux';
+import Loading from '../../../../pages/loading/Loading';
+import useDataCall from '../../../../hooks/useDataCall';
 
 const PManagement = () => {
+
+  const { getData } = useDataCall()
+  const { patient } = useSelector((state) => state.data)
+  let doctorInfo = []
+
+  useEffect(() => {
+      getData("patient")
+  }, [])
+
   return (
     <div>PManagement</div>
   )
--- Frontend/src/pages/panels/patientPanel/patientProfile/PatientProfile.jsx
+++ Frontend/src/pages/panels/patientPanel/patientProfile/PatientProfile.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import PProfile from '../../../../components/dashboard/patientDashboard/pProfile/PProfile'
+import useDataCall from '../../../../hooks/useDataCall'
+import { useSelector } from 'react-redux'
+import Loading from '../../../loading/Loading'
 
 const PatientProfile = () => {
+  
+  const { getData } = useDataCall()
+  const { patients } = useSelector((state) => state.data)
+  const { currentUser } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    getData("patients")
+  }, [])
+
+  const patientProfile = patients?.filter((item) => (currentUser === item.email))
+
   return (
-    <div>PatientProfile</div>
+    <>
+      {patientProfile ?
+        <div><PProfile {...patientProfile[0]}/></div>
+        : <Loading />}
+    </>
   )
 }
 
