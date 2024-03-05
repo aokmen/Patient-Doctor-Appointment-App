a190a3353cb55cc84cddab98b74f52353b58dc39
aokmen <abdullah.okmen.11@gmail.com>
2024-03-05 21:11:25 +0100

    isChecked's Boolean  added in doctor's and patient's model

--- Backend/src/models/patient.js
+++ Backend/src/models/patient.js
@@ -75,7 +75,11 @@ const PatientSchema = new mongoose.Schema({
     messageCount: {
         type: Number,
         default: 0
-    }
+    },
+    isChecked: {
+        type: Boolean,
+        default: true,
+    },
     
 }, { collection: 'patients', timestamps: true })
 
